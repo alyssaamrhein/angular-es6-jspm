9abd51656a1909b14c5ea9c1f45fed0068a3d5c8
Alyssa Amrhein <alyssa@gooee.com>
2015-12-21 16:45:56 -0500

    added comments to understand what happened later

--- app/app.js
+++ app/app.js
@@ -1,10 +1,14 @@
+// since angular is popular, there's already a shim for it and we're importing that beast.
 import angular from 'angular';
 
+
+// making a class es6 style.
 class MainController {
     constructor(){
         this.title= 'Changing this to see it change ok';
     }
 }
 
+// and assinging it to a controller which we'll access on the HTML page.
 angular.module('app', [])
     .controller('MainController', MainController);
